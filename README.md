6b4637b2defa264ef74152c12d69f5e6206bd294
D G <dazzag@gmail.com>
2021-01-25 19:49:39 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+http://www.goldsborough.me/go/2020/12/06/12-24-24-non-blocking_parallelism_for_services_in_go/
+
+
---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+   "sync"
+   "container/list"
+   "log"
+   "time"
+   "math/rand"
+   "context"
+)
+
+// Dummy request -- usually some Protobuf/Thrift struct
+type Request int
+
+type Service struct {
+    mu         sync.Mutex
+    queue      *list.List
+    sema       chan int
+    loopSignal chan struct{}
+}
+
+func (s *Service) EnqueueRequest(request Request) error {
+    s.mu.Lock()
+    defer s.mu.Unlock()
+    s.queue.PushBack(request)
+    log.Printf("Added request to queue with length %d\n", s.queue.Len())
+    s.tickleLoop()
+    return nil
+}
+
+func (s *Service) loop(ctx context.Context) {
+    log.Println("Starting service loop")
+    for {
+        select {
+        case <-s.loopSignal:
+            s.tryDequeue()
+        case <-ctx.Done():
+            log.Printf("Loop context cancelled")
+            return
+        }
+    }
+}
+
+func (s *Service) tryDequeue() {
+    s.mu.Lock()
+    defer s.mu.Unlock()
+    if s.queue.Len() == 0 {
+        return
+    }
+    select {
+    case s.sema <- 1:
+        request := s.dequeue()
+        log.Printf("Dequeued request %v\n", request)
+        go s.process(request)
+    default:
+        log.Printf("Received loop signal, but request limit is reached")
+    }
+}
+
+func (s *Service) dequeue() Request {
+    element := s.queue.Front()
+    s.queue.Remove(element)
+    return element.Value.(Request)
+}
+
+func (s *Service) process(request Request) {
+    defer s.replenish()
+    log.Printf("Processing request %v\n", request)
+    // Simulate work
+    <-time.After(time.Duration(rand.Intn(500)) * time.Millisecond)
+}
+
+func (s *Service) replenish() {
+    <-s.sema
+    log.Printf("Replenishing semaphore, now %d/%d slots in use\n", len(s.sema), cap(s.sema))
+    s.tickleLoop()
+}
+
+func (s *Service) tickleLoop() {
+    select {
+    case s.loopSignal <- struct{}{}:
+    default:
+    }
+}
+
+func NewService(ctx context.Context, requestLimit int) *Service {
+    service := &Service{
+        queue:      list.New(),
+        sema:       make(chan int, requestLimit),
+        loopSignal: make(chan struct{}, 1),
+    }
+    go service.loop(ctx)
+    return service
+}
+
+func main() {
+    ctx, cancel := context.WithCancel(context.Background())
+    defer cancel()
+    service := NewService(ctx, 3)
+    for i := 0; i < 10; i++ {
+        if err := service.EnqueueRequest(Request(i)); err != nil {
+            log.Fatalf("error sending request: %v", err)
+            break
+        }
+        <-time.After(time.Duration(rand.Intn(100)) * time.Millisecond)
+
+    }
+    for {
+        time.Sleep(time.Second)
+    }
+}
+
